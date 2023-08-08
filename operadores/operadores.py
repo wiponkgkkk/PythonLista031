d2fa84f15057e80527a40e0eb88609e151634e4e
William José <williamdelas222@gmail.com>
2023-08-08 09:08:34 -0300

    Commit - Aula interpolação e operadores

--- operadores/operadores.py
+++ operadores/operadores.py
@@ -1,6 +1,7 @@
 a = 3
 b = -4
 
+# Operadores Aritméticos
 soma = a + b
 sub = a - b
 mult = a * b
@@ -15,3 +16,30 @@ print(f"Divisão: { div }")
 print(f"Divisão de inteiros: { div_inteiros }")
 print(f"Cálculos dentro do print: { a * b }")
 print(f"Resto da divisão de 16 por a: { 16 % a }")
+
+# Operadores Relacionais
+
+print(f"a == b: { a == b }")
+print(f"a < 5: { a < 5 }")
+print(f"a > b: { a > b }")
+print(f"a <= 3: { a <= 3 }")
+print(f"a >= 4: { a >= 4 }")
+print(f"a != b: { a != b }")
+
+c = a != b
+print(f"c: {c}")
+print(f"tipo da var c: {type(c)}")
+
+
+# Operadores lógicos
+
+logic1 = True
+logic2 = False
+print(type(logic1))
+print(type(logic2))
+
+
+print(logic1)
+print(not logic1)
+print(logic1 or logic2)
+print(logic1 and logic2)
